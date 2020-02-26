8a8f2bc88e1456ffb845fdcde851f97e97e1ed9c
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-02-26 13:35:52 -0800

    add get users endpoint

--- api/api-router.js
+++ api/api-router.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const authRouter = require('../auth/auth-router.js');
+const userRouter = require('../user/user-router.js');
+const restricted = require('../auth/restricted.js');
 
 router.use('/', authRouter);
+router.use('/users', restricted, userRouter);
 
 module.exports = router;
--- user/user-router.js
+++ user/user-router.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
+const User = require('./user-model.js');
+
+router.get('/', async (req, res) => {
+  try {
+    res.status(200).json(await User.getAll());
+  }
+  catch ({ message, stack, code }) {
+    res.status(500).json({ message, stack, code });
+  }
+})
 
 module.exports = router;
